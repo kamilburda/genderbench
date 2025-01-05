eadc422953936dcb4beab811182fc0d95b900398
Matus Pikuliak <matus.pikuliak@gmail.com>
2025-01-05 11:47:29 +0000

    Visualization pipeline refactoring

--- src/gender_bench/viz/visualize.py
+++ src/gender_bench/viz/visualize.py
@@ -1,4 +1,5 @@
 import json
+from typing import Type
 
 from jinja2 import Environment, PackageLoader
 
@@ -15,13 +16,13 @@ from gender_bench.probes import (
     IsearProbe,
     JobsLumProbe,
 )
+from gender_bench.probing.probe import Probe
 
 env = Environment(loader=PackageLoader("gender_bench", "viz"))
 main_template = env.get_template("main.html")
 canvas_template = env.get_template("canvas.html")
 
-
-chart_data = {
+chart_config = {
     "decision_making": [
         (DiscriminationTamkinProbe, "max_diff"),
         (HiringAnProbe, "diff_acceptance_rate"),
@@ -49,42 +50,56 @@ chart_data = {
 }
 
 
-def aggregate_marks(marks: list[int]):
+def aggregate_marks(marks: list[int]) -> int:
+    """
+    Logic for mark aggregation. Currently we average the worst three results.
+    """
     marks = [mark for mark in marks if isinstance(mark, int)]
     return round(sum(sorted(marks)[-3:]) / 3)
 
 
-def section_mark(section_name, model_results):
+def section_mark(section_name: str, model_results: dict) -> int:
+    """
+    Aggregate marks of a model for the specified section.
+    """
     return aggregate_marks(
         [
             model_results["Marks"][probe_class.__name__][metric]["mark"]
-            for probe_class, metric in chart_data[section_name]
+            for probe_class, metric in chart_config[section_name]
         ]
     )
 
 
-def global_table_row(model_results):
-    row = [section_mark(section_name, model_results) for section_name in chart_data]
+def global_table_row(model_results: dict) -> list[str]:
+    """
+    Prepare row of aggregated marks for a single model's results.
+    """
+    row = [section_mark(section_name, model_results) for section_name in chart_config]
     row.append(aggregate_marks(row))
     row = [chr(mark + 65) for mark in row]
     return row
 
 
-def probe_data(probe_class, metric, logged_results):
+def prepare_chart_data(
+    probe_class: Type[Probe], metric: str, experiment_results: dict
+) -> dict:
+    """
+    Create a structure that is used to populate a single chart.
+    """
     probe_name = probe_class.__name__
     github_path = (
         "https://github.com/matus-pikuliak/gender_bench/tree/main/src/"
         + probe_class.__module__.rsplit(".", 1)[0].replace(".", "/")
     )
-    first_result = list(logged_results.values())[0]
+    first_result = list(experiment_results.values())[0]
     return {
         "description": first_result["Marks"][probe_name][metric]["description"],
         "tags": first_result["Marks"][probe_name][metric]["harm_types"],
-        "model_names": list(logged_results.keys()),
+        "model_names": list(experiment_results.keys()),
         "ranges": first_result["Marks"][probe_name][metric]["mark_ranges"],
         "intervals": [
             results["Marks"][probe_name][metric]["value"]
-            for results in logged_results.values()
+            for results in experiment_results.values()
         ],
         "probe": probe_name,
         "metric": metric,
@@ -92,50 +107,56 @@ def probe_data(probe_class, metric, logged_results):
     }
 
 
-def section_canvases(section_name, logged_results):
+def section_html(section_name: str, experiment_results: dict) -> str:
+    """
+    Create HTML renders for all the charts from a section.
+    """
     canvases_html = list()
-    for probe_class, metric in chart_data[section_name]:
-        html = canvas_template.render(
-            data=probe_data(probe_class, metric, logged_results)
+    canvases_html = [
+        canvas_template.render(
+            data=prepare_chart_data(probe_class, metric, experiment_results)
         )
-        canvases_html.append(html)
+        for probe_class, metric in chart_config[section_name]
+    ]
     return "".join(canvases_html)
 
 
-def create_visualizations(log_files, model_names):
+def render_visualization(log_files: list[str], model_names: list[str]) -> str:
+    """
+    Prepare an HTML render based on DefaultHarness log files. Models' names
+    must also be provided.
+    """
 
-    logged_results = {
+    experiment_results = {
         model_name: json.loads(open(log_file).readlines()[-1])
         for model_name, log_file in zip(model_names, log_files)
     }
 
     global_table = [
         [model_name, *global_table_row(model_results)]
-        for model_name, model_results in logged_results.items()
+        for model_name, model_results in experiment_results.items()
     ]
 
     rendered_sections = {
-        section_name: section_canvases(section_name, logged_results)
-        for section_name in chart_data
+        section_name: section_html(section_name, experiment_results)
+        for section_name in chart_config
     }
 
     rendered_html = main_template.render(
         global_table=global_table,
         rendered_sections=rendered_sections,
     )
-    with open("logs/out.html", "w") as f:
-        f.write(rendered_html)
+
+    return rendered_html
 
 
-if __name__ == "__main__":
-    logfiles = [
-        "logs/logs/87d269c2-a165-4599-a6d4-e36ed3ad590a.jsonl",
-        "logs/logs/a96da985-a48f-4e3b-934d-b26d904e6f9f.jsonl",
-        "logs/logs/65901fba-b50e-4460-8c0c-1517c3a54f51.jsonl",
-    ]
-    model_names = [
-        "Random 1",
-        "Random 2",
-        "Random 3",
-    ]
-    create_visualizations(log_files=logfiles, model_names=model_names)
+def create_html_visualization(
+    output_file_path: str, log_files: list[str], model_names: list[str]
+) -> str:
+    """
+    Save an HTML render based on DefaultHarness log files. Models' names
+    must also be provided.
+    """
+    html = render_visualization(log_files, model_names)
+    with open(output_file_path, "w") as f:
+        f.write(html)
