f9834152ba5ee4cafcc9f24340910470f85e0f36
Matúš Pikuliak <matus.pikuliak@gmail.com>
2025-01-22 18:32:02 +0000

    Allow reports to use results without CI

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_requirements(filename):
 
 setup(
     name="gender_bench",
-    version="0.1",
+    version="0.2",
     install_requires=read_requirements("requirements.txt"),
     package_dir={"": "src"},
     packages=find_namespace_packages(where="src"),
