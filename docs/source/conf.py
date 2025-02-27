9bf0348950049795fc800e520ec97acdb44cff96
Matúš Pikuliak <matus.pikuliak@gmail.com>
2025-02-27 20:36:37 +0000

    Add path to .readthedocs.yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,8 +31,3 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
-
-# -- Add src path for ReadMyDocs build ---------------------------------------
-import os
-import sys
-sys.path.insert(0, os.path.abspath("../../src"))
