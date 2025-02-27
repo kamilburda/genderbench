01e976ffa5deb208fbb94068bd7de31f35b50f4f
Matúš Pikuliak <matus.pikuliak@gmail.com>
2025-02-27 20:30:35 +0000

    Add src to docs config for ReadMyDocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,3 +31,8 @@ exclude_patterns = []
 
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+
+# -- Add src path for ReadMyDocs build ---------------------------------------
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../../src"))
