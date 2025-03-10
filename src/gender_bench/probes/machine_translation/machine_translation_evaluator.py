d8be0e3144d720a5954438b41783cdea3ec1892a
Kamil Burda <8431034+kamilburda@users.noreply.github.com>
2025-03-10 14:17:24 +0100

    Implement evaluator stub for machine translation probe

--- src/gender_bench/probes/machine_translation/machine_translation_evaluator.py
+++ src/gender_bench/probes/machine_translation/machine_translation_evaluator.py
@@ -1,3 +1,6 @@
+import random
+
+
 from gender_bench.probing.attempt import Attempt
 from gender_bench.probing.evaluator import Evaluator
 from gender_bench.probing.probe import Probe
@@ -12,4 +15,10 @@ class MachineTranslationEvaluator(Evaluator):
         super().__init__(probe=probe)
 
     def calculate_evaluation(self, attempt: Attempt) -> str:
-        return self.undetected
+        # TODO: Return one of the following based on `attempt.answer` and `attempt.prompt.metadata`:
+        # * "male" - if the gender detected from the translation is male
+        # * "female" - if the gender detected from the translation is female
+        # * "undetected" - if the gender could not be determined, the gender is neither male nor female, or the translation is not correct
+        possible_results = ["male", "female", "undetected"]
+
+        return possible_results[random.randint(0, len(possible_results))]
