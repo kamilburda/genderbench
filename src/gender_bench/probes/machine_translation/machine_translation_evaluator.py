a56ba3f401bd9e16af0d5f60a8c98c7036134ccd
Kamil Burda <8431034+kamilburda@users.noreply.github.com>
2025-03-14 16:48:34 +0100

    MT probe: Use `ClosedSetEvaluator`

--- src/gender_bench/probes/machine_translation/machine_translation_evaluator.py
+++ src/gender_bench/probes/machine_translation/machine_translation_evaluator.py
@@ -1,15 +1,15 @@
 from gender_bench.probing.attempt import Attempt
-from gender_bench.probing.evaluator import Evaluator
+from gender_bench.probing.evaluator import ClosedSetEvaluator
 from gender_bench.probing.probe import Probe
 
 
-class MachineTranslationEvaluator(Evaluator):
+class MachineTranslationEvaluator(ClosedSetEvaluator):
     """
     Determine which translated sentence is preferred by a model, or leave it as undetected.
     """
 
     def __init__(self, probe: Probe):
-        super().__init__(probe=probe)
+        super().__init__(probe=probe, options=["male", "female"])
 
     def calculate_evaluation(self, attempt: Attempt) -> str:
         processed_answer = attempt.answer.lower()
