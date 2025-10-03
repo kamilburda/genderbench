8f222769b2460f8834026b71ef82fa0f8cfe8b8e
Kamil Burda <8431034+kamilburda@users.noreply.github.com>
2025-10-03 17:29:50 +0200

    Fix probes failing for Azure OpenAI models due to inappropriate content

--- genderbench/generators/azure_open_ai_async_api.py
+++ genderbench/generators/azure_open_ai_async_api.py
@@ -1,4 +1,7 @@
+import warnings
+
 from openai import AsyncAzureOpenAI
+from openai import BadRequestError
 
 from genderbench.generators.async_api import AsyncApiGenerator
 
@@ -6,7 +9,23 @@ from genderbench.generators.async_api import AsyncApiGenerator
 class AzureOpenAiAsyncApiGenerator(AsyncApiGenerator):
     """`AzureOpenAiAsyncApiGenerator` is the `AsyncApiGenerator` subclass that
     is able to invoke the Azure OpenAI API.
+
+    Args:
+        ignore_content_filter_errors (bool): If ``True``, error responses
+            related to content filtering by the LLM are ignored. Defaults
+            to ``False``.
     """
+
+    def __init__(
+            self,
+            model: str,
+            api_key: str,
+            ignore_content_filter_errors: bool = False,
+            **kwargs,
+    ):
+        self._ignore_content_filter_errors = ignore_content_filter_errors
+
+        super().__init__(model, api_key, **kwargs)
 
     def initialize_client(self, base_url, api_key, **kwargs):
         return AsyncAzureOpenAI(
@@ -16,14 +35,22 @@ class AzureOpenAiAsyncApiGenerator(AsyncApiGenerator):
         )
 
     async def call_generation_api(self, text: str) -> str:
-        completion = await self.client.chat.completions.create(
-            model=self.model,
-            messages=[{"role": "user", "content": text}],
-            max_tokens=self.max_tokens,
-            temperature=self.temperature,
-            top_p=self.top_p,
-        )
-        answer = completion.choices[0].message.content
-        if answer is None:  # Google AI Studio sometimes returns None
-            answer = ""
-        return answer
+        try:
+            completion = await self.client.chat.completions.create(
+                model=self.model,
+                messages=[{"role": "user", "content": text}],
+                max_tokens=self.max_tokens,
+                temperature=self.temperature,
+                top_p=self.top_p,
+            )
+        except Exception as e:
+            if self._ignore_content_filter_errors and isinstance(e, BadRequestError) and e.code == 'content_filter':
+                warnings.warn(f"The following prompt was filtered due to content management policy:\n{text}")
+                return ""
+            else:
+                raise
+        else:
+            answer = completion.choices[0].message.content
+            if answer is None:  # Google AI Studio sometimes returns None
+                answer = ""
+            return answer
